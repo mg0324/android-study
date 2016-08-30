768bce2639d0241fcc54d68048b4eb5c077c4f54
macgang <1092017732@qq.com>
2016-08-30 09:59:34 +0800

    添加第一个demo for activity的打开另一个activity跳转到淘宝

---
+++ helloAndroid/app/src/main/java/com/example/meigang/helloandroid/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.meigang.helloandroid;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.net.URI;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(MainActivity.this,OtherActivity.class));
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.taobao.com")));
+            }
+        });
+    }
+}
